d986011d93ebdd3464dd2c84fff375ac5135f36d
jangyoonhee <yh0109094@gmail.com>
2022-01-18 22:09:08 +0000

    test

---
+++ src/components/card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './css/basic.css';
+
+const card = ({form, children}) => {
+  return (
+    <main className="card-template">
+      <div className="card-title">
+        TODAY
+      </div>
+      <section className="form-wrapper">
+        {form}
+      </section>
+      <section className="todos-wrapper">
+        { children }
+      </section>
+    </main>
+  );
+};
+
+export default card;
